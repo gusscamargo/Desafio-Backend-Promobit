bd044497695a6708a71f46a12407d02ee941d875
gusscamargo <guga.power@hotmail.com>
2021-12-21 03:55:42 -0300

    Se usuario estiver autenticado, ele não mais conseguirar acessar a pagina de login

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller{
     public function __invoke(){
-        // return view("index");
+        
+        if(Auth::check()){
+            return redirect("/home");
+        }
+
         return view("login.auth");
     }
 }
