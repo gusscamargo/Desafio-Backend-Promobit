e5acbf6749b1f38b7c0c4e72f6bf148eb4ae4850
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2022-01-27 20:16:44 -0300

    Adicionando ';'

--- sql/relevanciaProdutos.sql
+++ sql/relevanciaProdutos.sql
@@ -1,1 +1,1 @@
-SELECT `tag`.`name` AS nome, (SELECT COUNT(*) FROM `product_tag` WHERE `product_tag`.`tag_id` = `tag`.`id`) AS num_produtos_relacionados from `tag` ORDER BY num_produtos_relacionados DESC
+SELECT `tag`.`name` AS nome, (SELECT COUNT(*) FROM `product_tag` WHERE `product_tag`.`tag_id` = `tag`.`id`) AS num_produtos_relacionados from `tag` ORDER BY num_produtos_relacionados DESC;
